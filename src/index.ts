f7055e8e544fd690e15afaf92c345b045b70fb47
acechl <1063256587@qq.com>
2017-12-11 08:15:23 +0800

    first

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// function hello(compiler: string) {
+//     console.log(`Hello from ${compiler}`);
+// }
+// hello("TypeScript");
+
+import {sayHello} from "./greet";
+console.log(sayHello("TypeScript"));
